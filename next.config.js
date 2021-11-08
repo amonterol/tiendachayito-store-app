5efde13061f334f5e973c7073dfd790dfdbbd89c
Adrian Montero <abmontero@yahoo.com>
2021-11-07 19:44:36 -0600

    fin

--- next.config.js
+++ next.config.js
@@ -6,9 +6,7 @@ module.exports = {
       "mongodb+srv://admin:DHlWbzMLttsgNLOi@cluster0.9mg5a.mongodb.net/tienda-chayito?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "b&7nvedd2A8Z-}NmQL)Q%",
     REFRESH_TOKEN_SECRET: "M7Uy^9$-fnu=k;&#f-F}rb(ztt/fUbZ",
-    CLOUDINARY_NAME: "abmontero",
-    CLOUDINARY_API_KEY: "255914831661626",
-    CLOUDINARY_API_SECRET: "qJnxSNXBCRr9sgNVfdYA5wQtt34",
+
     CLOUD_UPDATE_PRESET: "tienda",
     CLOUD_NAME: "abmontero",
     CLOUD_API: "https://api.cloudinary.com/v1_1/abmontero/image/upload",
